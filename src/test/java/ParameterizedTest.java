feed64cc6501c90ef6a9f24b1ba08686ebf10fc6
Artyom Melnikov <artyom.melnikov@aurea.com>
2017-06-05 15:49:23 +0300

    Test with specific test name

---
+++ src/test/java/ParameterizedTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * Created by APXEOLOG on 05.06.2017.
+ */
+
+@RunWith(Parameterized.class)
+public class ParameterizedTest {
+    @Parameterized.Parameters(name = "testEscapingMechanism with parameter \"{0}\"")
+    public static List<String> data() {
+        return Arrays.asList("*a\\Qb*", "a\\Qb");
+    }
+
+    private String input;
+
+    public ParameterizedTest(String input) {
+        this.input = input;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(input, input);
+    }
+}
