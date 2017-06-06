aa99d91273b915596626ad69e283386faf932958
Artyom Melnikov <artyom.melnikov@aurea.com>
2017-06-06 13:28:20 +0300

    Test with specific test name

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,5 +8,6 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        // This is for sure not a master branch
     }
 }
